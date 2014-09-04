11266f58e7c6154dd2445968047e1f7875cabe0c
Lionel Roubeyrie <lionel.roubeyrie@codinux.fr>
2014-09-04 17:45:01 +0200

    Utilisation du QAbstractItemModel pour l'explorateur et insertion des noms de colonnes lors d'un double click

--- src/explorermodel.py
+++ src/explorermodel.py
@@ -1,14 +1,13 @@
-from PyQt4 import QtCore, QtGui
-import sys
-import resources_rc
+from PyQt4 import QtCore
 
 # Source : PyQt4 Model View Tutorial by Yasin Uludag on Youtube
 
 class Node(object):
-    def __init__(self, name, parent=None):
+    def __init__(self, name, parent=None, icon=None):
         self._name = name
         self._children = []
         self._parent = parent
+        self._icon = icon
         if parent is not None:
             parent.addChild(self)
 
@@ -47,6 +46,9 @@ class Node(object):
     def parent(self):
         return self._parent
 
+    def icon(self):
+        return self._icon
+
     def row(self):
         if self._parent is not None:
             return self._parent._children.index(self)
@@ -67,73 +69,81 @@ class Node(object):
         return self.log()
 
 
+class GenericNode(Node):
+    def __init__(self, name, parent=None, icon=None):
+        super(GenericNode, self).__init__(name, parent, icon)
+
+    def typeInfo(self):
+        return "GENERIC"
+
+
 class SchemaNode(Node):
-    def __init__(self, name, parent=None):
-        super(SchemaNode, self).__init__(name, parent)
+    def __init__(self, name, parent=None, icon=None):
+        super(SchemaNode, self).__init__(name, parent, icon)
 
     def typeInfo(self):
         return "SCHEMA"
 
 
 class TableNode(Node):
-    def __init__(self, name, parent=None):
-        super(TableNode, self).__init__(name, parent)
+    def __init__(self, name, parent=None, icon=None):
+        super(TableNode, self).__init__(name, parent, icon)
 
     def typeInfo(self):
         return "TABLE"
 
 
 class ViewNode(Node):
-    def __init__(self, name, parent=None):
-        super(ViewNode, self).__init__(name, parent)
+    def __init__(self, name, parent=None, icon=None):
+        super(ViewNode, self).__init__(name, parent, icon)
 
     def typeInfo(self):
         return "VIEW"
 
 
 class ColumnNode(Node):
-    def __init__(self, name, parent=None):
-        super(ColumnNode, self).__init__(name, parent)
+    def __init__(self, name, parent=None, icon=None):
+        super(ColumnNode, self).__init__(name, parent, icon)
 
     def typeInfo(self):
         return "COLUMN"
 
 
 class IndexNode(Node):
-    def __init__(self, name, parent=None):
-        super(IndexNode, self).__init__(name, parent)
+    def __init__(self, name, parent=None, icon=None):
+        super(IndexNode, self).__init__(name, parent, icon)
 
     def typeInfo(self):
         return "INDEX"
 
 
 class SequenceNode(Node):
-    def __init__(self, name, parent=None):
-        super(SequenceNode, self).__init__(name, parent)
+    def __init__(self, name, parent=None, icon=None):
+        super(SequenceNode, self).__init__(name, parent, icon)
 
     def typeInfo(self):
         return "SEQUENCE"
 
 
 class MaterializedView(Node):
-    def __init__(self, name, parent=None):
-        super(MaterializedView, self).__init__(name, parent)
+    def __init__(self, name, parent=None, icon=None):
+        super(MaterializedView, self).__init__(name, parent, icon)
 
     def typeInfo(self):
-        return "MATERIALIZEDVIEW"
+        return "MATERIALIZED VIEW"
 
 
 class ForeignTable(Node):
-    def __init__(self, name, parent=None):
-        super(ForeignTable, self).__init__(name, parent)
+    def __init__(self, name, parent=None, icon=None):
+        super(ForeignTable, self).__init__(name, parent, icon)
 
     def typeInfo(self):
-        return "FOREIGNTABLE"
+        return "FOREIGN TABLE"
 
 
 class Special(Node):
-    def __init__(self, name, parent=None):
-        super(Special, self).__init__(name, parent)
+    def __init__(self, name, parent=None, icon=None):
+        super(Special, self).__init__(name, parent, icon)
 
     def typeInfo(self):
         return "SPECIAL"
@@ -177,13 +187,7 @@ class ExplorerModel(QtCore.QAbstractItemModel):
                 return node.name()
         if role == QtCore.Qt.DecorationRole:
             if index.column() == 0:
-                typeInfo = node.typeInfo()
-                if typeInfo == "LIGHT":
-                    return QtGui.QIcon(QtGui.QPixmap(":/Light.png"))
-                if typeInfo == "TRANSFORM":
-                    return QtGui.QIcon(QtGui.QPixmap(":/Transform.png"))
-                if typeInfo == "CAMERA":
-                    return QtGui.QIcon(QtGui.QPixmap(":/Camera.png"))
+                return node.icon()
 
     def setData(self, index, value, role=QtCore.Qt.EditRole):
         """INPUTS: QModelIndex, QVariant, int (flag)"""
@@ -201,7 +205,7 @@ class ExplorerModel(QtCore.QAbstractItemModel):
         """
         if role == QtCore.Qt.DisplayRole:
             if section == 0:
-                return "Scenegraph"
+                return "Database"
             else:
                 return "Typeinfo"
 
@@ -250,13 +254,26 @@ class ExplorerModel(QtCore.QAbstractItemModel):
                 return node
         return self._rootNode
 
-    def onInsertColumn(self, position, columns, parent=QtCore.QModelIndex()):
+    def insertRows(self, position, rows, parent=QtCore.QModelIndex()):
+        """INPUTS: int, int, QModelIndex"""
         parentNode = self.getNode(parent)
-        if parentNode.typeInfo in ("TABLE", "VIEW", "MATERIALIZEDVIEW") and len(columns)>0:
-            self.beginInsertRows(parent, position, position + columns - 1)
+        self.beginInsertRows(parent, position, position + rows - 1)
+        for row in range(rows):
+            childCount = parentNode.childCount()
+            childNode = Node("untitled" + str(childCount))
+            success = parentNode.insertChild(position, childNode)
+        self.endInsertRows()
+        return success
+
+    # DON'T use insertColumns as it's a QAbstractItemModel function
+    def insertColumnNames(self, position, columns, parent=QtCore.QModelIndex()):
+        """INPUTS: int, int, QModelIndex"""
+        parentNode = self.getNode(parent)
+        if parentNode.typeInfo() in ("TABLE", "VIEW", "MATERIALIZED VIEW") and len(columns) > 0:
+            self.beginInsertRows(parent, position, position + len(columns) - 1)
             for column in columns:
                 # childCount = parentNode.childCount()
-                childNode = ColumnNode(column)
+                childNode = ColumnNode(column[0])
                 success = parentNode.insertChild(position, childNode)
             self.endInsertRows()
             return success
@@ -271,32 +288,3 @@ class ExplorerModel(QtCore.QAbstractItemModel):
             success = parentNode.removeChild(position)
         self.endRemoveRows()
         return success
-
-
-if __name__ == '__main__':
-    app = QtGui.QApplication(sys.argv)
-    # app.setStyle("plastique")
-
-    rootNode = Node("Hips")
-    childNode0 = TransformNode("RightPirateLeg", rootNode)
-    childNode1 = Node("RightPirateLeg_END", childNode0)
-    childNode2 = CameraNode("LeftFemur", rootNode)
-    childNode3 = Node("LeftTibia", childNode2)
-    childNode4 = Node("LeftFoot", childNode3)
-    childNode5 = LightNode("LeftFoot_END", childNode4)
-
-    print rootNode
-
-    model = ExplorerModel(rootNode)
-
-    treeView = QtGui.QTreeView()
-    treeView.show()
-
-    treeView.setModel(model)
-
-    rightPirateLeg = model.index(0, 0, QtCore.QModelIndex())
-
-    model.insertRows(1, 5, rightPirateLeg)
-    model.insertLights(1, 5, rightPirateLeg)
-
-    sys.exit(app.exec_())
--- src/mainexplorer.py
+++ src/mainexplorer.py
@@ -1,30 +1,37 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 from ui.forms.explorerui import ExplorerUI
 from src import db
+import explorermodel as em
+
+
 __author__ = 'lionel'
 
 
-
 class MainExplorer(ExplorerUI):
-
     def __init__(self, *args, **kwargs):
         self.icons = {
-            'columns': QtGui.QIcon(QtGui.QPixmap(':/column.png')),
-            'tables': QtGui.QIcon(QtGui.QPixmap(':/table.png')),
-            'views': QtGui.QIcon(QtGui.QPixmap(':/view.png')),
-            'indexes': QtGui.QIcon(QtGui.QPixmap(':/index.png')),
-            'sequences': QtGui.QIcon(QtGui.QPixmap(':/sequence.png')),
-            'materialized views': QtGui.QIcon(QtGui.QPixmap(':/materialized_view.png')),
-            'foreign tables': QtGui.QIcon(QtGui.QPixmap(':/foreign_table.png')),
-            'specials': QtGui.QIcon(QtGui.QPixmap(':/view.png')),
-            'schema': QtGui.QIcon(QtGui.QPixmap(':/schema.png')),
-            }
+            'COLUMN': QtGui.QIcon(QtGui.QPixmap(':/column.png')),
+            'TABLE': QtGui.QIcon(QtGui.QPixmap(':/table.png')),
+            'VIEW': QtGui.QIcon(QtGui.QPixmap(':/view.png')),
+            'INDEX': QtGui.QIcon(QtGui.QPixmap(':/index.png')),
+            'SEQUENCE': QtGui.QIcon(QtGui.QPixmap(':/sequence.png')),
+            'MATERIALIZED VIEW': QtGui.QIcon(QtGui.QPixmap(':/materialized_view.png')),
+            'FOREIGN TABLE': QtGui.QIcon(QtGui.QPixmap(':/foreign_table.png')),
+            'SPECIAL': QtGui.QIcon(QtGui.QPixmap(':/view.png')),
+            'SCHEMA': QtGui.QIcon(QtGui.QPixmap(':/schema.png')),
+            'DATABASE': QtGui.QIcon(QtGui.QPixmap(':/database.png')),
+        }
+
         self.database = kwargs.pop("database")
         super(MainExplorer, self).__init__(*args, **kwargs)
+        self.rootNode = em.Node(self.database, parent=None, icon=self.icons['DATABASE'])
+
+        self.model = em.ExplorerModel(self.rootNode)
         self.view = self.uiExplorerTree
-        self.model = QtGui.QStandardItemModel()
+        self.view.setModel(self.model)
+
         self.setupExplorer()
         self.setupToolBar()
 
@@ -32,20 +39,20 @@ class MainExplorer(ExplorerUI):
         self.uiExplorerTree.doubleClicked.connect(self.onDoubleClick)
 
     def setupExplorer(self):
+        self.view.reset()
+
         query = """SELECT
             n.nspname as "schema",
             CASE c.relkind
-                WHEN 'r' THEN 'tables'
-                WHEN 'v' THEN 'views'
-                WHEN 'm' THEN 'materialized views'
-                WHEN 'i' THEN 'indexes'
-                WHEN 'S' THEN 'sequences'
-                WHEN 's' THEN 'specials'
-                WHEN 'f' THEN 'foreign tables'
+                WHEN 'r' THEN 'TABLE'
+                WHEN 'v' THEN 'VIEW'
+                WHEN 'm' THEN 'MATERIALIZED VIEW'
+                WHEN 'i' THEN 'INDEX'
+                WHEN 'S' THEN 'SEQUENCE'
+                WHEN 's' THEN 'SPECIAL'
+                WHEN 'f' THEN 'FOREIGN TABLE'
             END as "type",
-            c.relname as "name",
-            pg_catalog.pg_get_userbyid(c.relowner) as "owner",
-            c.oid
+            c.relname as "name"
         FROM pg_catalog.pg_class c
         LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
         WHERE c.relkind IN ('r','v','m','S','f','')
@@ -53,40 +60,44 @@ class MainExplorer(ExplorerUI):
             AND n.nspname <> 'information_schema'
             AND n.nspname !~ '^pg_toast'
         ORDER BY 1,2,3"""
-
         conn = self.database.newConnection()
         headers, res, status = db.execute(conn, query)
         conn.close()
-        self.view.reset()
+
         self.view.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
-        self.model.clear()
-        self.model.setHorizontalHeaderLabels(['Database'])
-        self.view.setModel(self.model)
         self.view.setUniformRowHeights(True)
         self.view.setAnimated(True)
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
         # populate data
         parentSchema = None
         parentType = None
-        for schema, type_, name, owner, oid in res:
-            if schema != parentSchema: # On changing/first schema in the list
-                schemaItem = QtGui.QStandardItem(self.icons['schema'], schema)
-                parentSchema = schema #Set the schema name as the parent
-                parentType = None #Reset the type
-                self.model.appendRow(schemaItem)
-            if type_ != parentType: #On changing/first type in the list
-                parentType = type_ #Set the type as the parent type
-                typeItem = QtGui.QStandardItem(self.icons[type_], type_)
-                schemaItem.appendRow(typeItem)
-            nameItem = QtGui.QStandardItem(self.icons[type_], name)
-            # ownerItem = QtGui.QStandardItem(owner)
-            # oidItem = QtGui.QStandardItem(oid)
-            # typeItem.appendRow([nameItem, ownerItem, oidItem])
-            typeItem.appendRow(nameItem)
-            # if type_ in ('tables', 'views', 'materialized views'):
-            #     for colName in self.getColumnsName(schema, name):
-            #         colNameItem = QtGui.QStandardItem(self.icons['columns'], colName[0])
-            #         nameItem.appendRow(colNameItem)
+        for schema, type_, name in res:
+            if schema != parentSchema:  # On changing/first schema in the list
+                schemaItem = em.SchemaNode(schema, parent=self.rootNode, icon=self.icons['SCHEMA'])
+                parentSchema = schema  # Set the schema name as the parent
+                parentType = None  # Reset the type
+            if type_ != parentType:  # On changing/first type in the list
+                parentType = type_  # Set the type as the parent type
+                typeItem = em.GenericNode(type_, parent=schemaItem, icon=self.icons[type_])
+            if type_ == "TABLE":
+                nameItem = em.TableNode(name, typeItem, icon=self.icons["TABLE"])
+            if type_ == "SEQUENCE":
+                nameItem = em.SequenceNode(name, typeItem, icon=self.icons["SEQUENCE"])
+            if type_ == "VIEW":
+                nameItem = em.ViewNode(name, typeItem, icon=self.icons["VIEW"])
+            if type_ == "MATERIALIZED VIEW":
+                nameItem = em.MaterializedView(name, typeItem, icon=self.icons["MATERIALIZED VIEW"])
+            if type_ == "INDEX":
+                nameItem = em.IndexNode(name, typeItem, icon=self.icons["INDEX"])
+            if type_ == "FOREIGN TABLE":
+                nameItem = em.ForeignTable(name, typeItem, icon=self.icons["FOREIGN TABLE"])
+            if type_ == "SPECIAL":
+                nameItem = em.Special(name, typeItem, icon=self.icons["SPECIAL"])
+
+                # if type_ in ('tables', 'views', 'materialized views'):
+                #     for colName in self.getColumnsName(schema, name):
+                #         colNameItem = QtGui.QStandardItem(self.icons['columns'], colName[0])
+                #         nameItem.appendRow(colNameItem)
 
     def getColumnsName(self, schema, table):
         query = """SELECT
@@ -121,8 +132,12 @@ class MainExplorer(ExplorerUI):
         self.uiExplorerTree.expandAll()
 
     def onDoubleClick(self, index):
-        print(index.sibling(index.row(), 0).data().toString())
-        print(index.sibling(index.row(), 2).data())
-        # row = index.row()
-        # t = unicode(self.model.item(row, 0).text())
-        # print(t)
+        parentNode = self.model.getNode(index)
+        if parentNode.typeInfo() in ("TABLE", "VIEW", "MATERIALIZED VIEW"):
+            childCount = parentNode.childCount()
+            if childCount > 0:
+                self.model.removeRows(0, childCount, index)
+            parentIndex = self.model.parent(self.model.parent(index))  # Needed to jump the generic node
+            schemaNode = self.model.getNode(parentIndex)
+            columns = self.getColumnsName(schemaNode.name(), parentNode.name())
+            self.model.insertColumnNames(0, columns, index)
